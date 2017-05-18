5cf3a30687ef4450fced35b292b5fa465ff1b12d
Dana Kulp <dkulp23@gmail.com>
2017-05-18 01:08:20 -0700

    Complete tutorial. The potential of this framework is awesome.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 'use strict';
 
 var m = require('mithril');
-var UserList = require('./views/UserList');
 
-m.mount(document.body, UserList);
+var UserList = require('./views/UserList');
+var UserForm = require('./views/UserForm');
+var Layout = require('./views/Layout');
+
+m.route(document.body, '/list', {
+  '/list': {
+    render: function() {
+      return m(Layout, m(UserList))
+    }
+  },
+  '/edit/:id': {
+    render: function(vnode) {
+      return m(Layout, m(UserForm, vnode.attrs))
+    }
+  }
+});
--- src/models/User.js
+++ src/models/User.js
@@ -13,6 +13,26 @@ var User = {
     .then(function(result) {
       User.list = result.data;
     });
+  },
+  current: {},
+  load: function(id) {
+    return m.request({
+      method: 'GET',
+      url: 'https://rem-rest-api.herokuapp.com/api/users/:id',
+      data: { id: id },
+      withCredentials: true
+    })
+    .then(function(result) {
+      User.current = result;
+    });
+  },
+  save: function() {
+    return m.request({
+      method: 'PUT',
+      url: 'https://rem-rest-api.herokuapp.com/api/users/:id',
+      data: User.current,
+      withCredentials: true
+    })
   }
 };
 
